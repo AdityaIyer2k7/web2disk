dc4d834015717deb5534ceba49a7742762b172e9
DrSparky-2007 <adityaiyer2007@gmail.com>
2021-02-18 04:36:01 +0000

    It works now

---
+++ main.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import os
+
+fileloc = Path(__file__).parent
+
+def get(url, dir=os.path.join(fileloc, "fromURL"),
+        filename="target", ext="pdf"):
+    from urllib.request import urlopen
+    data = urlopen(url).read()
+
+    if not os.path.exists(dir):
+        raise OSError("Target directory does not exist")
+
+    file = f"{filename}.{ext}"
+    file = os.path.join(dir, file)
+
+    if not os.path.exists(file):
+        open(file, mode='x').close()
+    with open(file, mode="wb") as fl:
+        fl.write(data)
+
+if __name__ == "__main__":
+    get(input("Url of a pdf: "))
